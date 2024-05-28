d3e4f403a9e9e44ce1bfea5d4ee070a94a2a9aec
liwenxiang <liwenxiang@cheerlandgroup.com>
2024-05-28 17:42:43 +0800

    删除svg

--- src/components/index.vue
+++ src/components/index.vue
@@ -3,11 +3,6 @@
   <n-grid class="notice" x-gap="12" :cols="1" :style="theme()">
     <n-gi>
       <div style="display: flex;flex-direction: row-reverse;margin: 10px 10px 0 0;">
-        <n-switch v-model:value="active" size="medium">
-          <template #icon>
-            <img src="../assets/DarkModeOutlined.svg" alt="Description">
-          </template>
-        </n-switch>
         <n-switch :default-value="isDarkTheme" @update:value="(v) => $emit('changeIsDarkTheme', v)" >
           <template #checked-icon>
             <DarkModeOutlined />
